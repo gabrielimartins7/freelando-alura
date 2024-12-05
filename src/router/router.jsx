0d1a7bd8c6e6faa312f352eb868e9773e5414a06
gabrielimartins7 <martins.gabrieli.07.almeida@outlook.com>
2024-12-05 16:46:56 -0300

    Criando o roteamento

---
+++ src/router/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom"
+import { SignUp } from "../pages/SignUp";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <SignUp />,
+        children: [],
+    },
+]);
